d81a9efc7d0b3c394f38aabcbae8900210e9daed
semirakemal <ksemira225@gmail.com>
2026-05-12 21:54:39 +0300

    added about page and fixed form navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import './styles/global.css';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Projects from './sections/Projects';
-import About from './sections/About';
-import WhatWeDo from './sections/WhatWeDo';
-import Nujum from './sections/Nujum';
-import Leadership from './sections/Leadership';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import "./styles/global.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Projects from "./sections/Projects";
+import About from "./sections/About";
+import WhatWeDo from "./sections/WhatWeDo";
+import Nujum from "./sections/Nujum";
+import Leadership from "./sections/Leadership";
+import Form from "./components/common/Form";
 
 function ScrollToHash() {
   const location = useLocation();
-  
+
   useEffect(() => {
     if (location.hash) {
       const element = document.querySelector(location.hash);
       if (element) {
         setTimeout(() => {
-          element.scrollIntoView({ behavior: 'smooth' });
+          element.scrollIntoView({ behavior: "smooth" });
         }, 100);
       }
     }
   }, [location]);
-  
+
   return null;
 }
 
@@ -41,6 +42,7 @@ function App() {
         <Route path="/what-we-do" element={<WhatWeDo />} />
         <Route path="/nujum" element={<Nujum />} />
         <Route path="/leadership" element={<Leadership />} />
+        <Route path="/form" element={<Form />} />
       </Routes>
       <Footer />
     </BrowserRouter>
